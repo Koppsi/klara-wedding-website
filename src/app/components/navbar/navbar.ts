1b48daad0a9a95f87dd64509e0744bc7d6eb6d17
Simon Kopp <simon.kopp@uni-ulm.de>
2026-01-19 21:51:48 +0100

    angular 21

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -26,13 +26,13 @@
 // }
 
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
